41d674d2765ee3a0a14e328f514dbbfb845e4d67
SardarBadar <dataentrybadar@gmail.com>
2022-06-30 15:40:13 +0500

    fix html cards

--- source/content.ts
+++ source/content.ts
@@ -1,15 +1,25 @@
+import { fbCard } from './card';
 import optionsStorage from './options-storage';
+import { Posts } from './models/post';
 
 async function init() {
-	const options = await optionsStorage.getAll();
-	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
-	const text = options.text;
-	const notice = document.createElement('div');
-	notice.innerHTML = text;
-	document.body.append(notice);
-	notice.id = 'text-notice';
-	notice.style.border = '2px solid ' + color;
-	notice.style.color = color;
+	// setTimeout(function() {
+	// 	//your code to be executed after 1 second
+
+	// 	const roleFeed = document.querySelector('[role=feed]') as HTMLDivElement;
+	// roleFeed.innerHTML='<p>Hi I am Injected In feeds</p>'+roleFeed.innerHTML
+	//   }, 60000);
+	window.onload = function () {
+		const post = new Posts('<p>Hi I am Injected In feeds</p>', 'Dr Ah', 'Injected');
+		const injected=fbCard(post);
+		console.log(injected);
+		const roleFeed = document.querySelector('[role=feed]') as HTMLDivElement;
+		roleFeed.innerHTML=injected+roleFeed.innerHTML;
+
+	}
+
+	
+	
 }
 
 init();
---
+++ source/models/post.ts
@@ -0,0 +1,12 @@
+export class Posts {
+    contentHtml:String;
+    name:string;
+    time:string;
+    constructor(contentHtml:string,name:string,time:string) {
+        this.contentHtml=contentHtml;
+        this.name=name;
+        this.time=time;
+    }
+    
+    
+}
