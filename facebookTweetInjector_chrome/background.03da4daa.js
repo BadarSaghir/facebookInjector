0c0176722cd8e21ad0e92ae69bf5cbe7f19d36ed
SardarBadar <dataentrybadar@gmail.com>
2022-07-12 13:08:47 +0500

    build firefox

--- facebookTweetInjector_chrome/background.03da4daa.js
+++
@@ -1,9 +0,0 @@
-console.log("background"), chrome.runtime.onMessage.addListener(((request, sender, sendResponse) => {
-  console.log("getting req");
-  var url = request.url;
-  return fetch(url).then((res => (console.log(res), res.json()))).then((json => {
-    console.log("getting json :", json), sendResponse(json);
-  })).catch((e => {
-    console.log("error due to ", e);
-  })), console.log("finish req"), !0;
-}));
